ecadab298bd0045056089705f86b0ebac6000ac5
manmeetdevgun <manmeet.devgun@nagarro.com>
2021-08-16 10:46:18 +0530

    Inital Commit

---
+++ src/main/java/com/nagp/manmeet/jenkins/nagpdemojenkins/NagpDemoJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.nagp.manmeet.jenkins.nagpdemojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagpDemoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagpDemoJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagp/manmeet/jenkins/nagpdemojenkins/NagpDemoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagp.manmeet.jenkins.nagpdemojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagpDemoJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
